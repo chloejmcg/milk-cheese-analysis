b01dd5240cd531a6fb258e8cc01888cedb490b9b
chloejmcgov <cjm423@cornell.edu>
2025-04-18 09:52:06 -0400

    colors changed to PINK!

--- code/colors.R
+++ code/colors.R
@@ -1,6 +1,6 @@
 # Setting colors for cheese type 
 cheese_colors <- c(
-  "Colonial" = "dodgerblue4",
-  "Feta-type" = "dodgerblue2",
-  "Fresh" = "#D9CC3C",
-  "Pecorino-type" = "#00ADA7")
+  "Colonial" = "lightpink1",
+  "Feta-type" = "hotpink",
+  "Fresh" = "deeppink3",
+  "Pecorino-type" = "deeppink4")
